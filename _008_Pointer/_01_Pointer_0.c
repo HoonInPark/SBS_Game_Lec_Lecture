4c7b21bcee11685046ecd93b9cfb97073484125f
hooninpark <hooninpark@gmail.com>
2023-08-05 12:20:51 +0900

    example of error that returns falls val

---
+++ _008_Pointer/_01_Pointer_0.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// 배열을 이용하여 두 정수를 더하는 함수 (포인터를 사용하지 않음)
+void addNumbers(int a[], int b[], int result[], int size) {
+    for (int i = 0; i < size; i++) {
+        result[i] = a[i] + b[i];
+    }
+}
+
+void main() {
+    int array1[5] = { 1, 2, 3, 4, 5 };
+    int array2[5] = { 5, 4, 3, 2, 1 };
+    int sumArray[5];
+
+    // 두 배열을 더함
+    addNumbers(array1, array2, sumArray, 5);
+
+    // 결과 출력
+    printf("Sum Array: ");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", sumArray[i]);
+    }
+}
--- _008_Pointer/_02_array_and_pointer.c
+++ _008_Pointer/_02_array_and_pointer.c
@@ -10,21 +10,21 @@
 void main()
 {
 	int kor[] = { 99, 100, 88, 78, 89 };
-	int* parr = kor;
+	int* pArr = kor;
 
 	// %p는 16진수로 포인터 변수의 값을 출력할 때 사용
 	printf("%p\n", kor);
 	printf("%p\n", &kor[0]);
 
-	// parr은 배열의 시작위치를 가리킨다.
+	// pArr은 배열의 시작위치를 가리킨다.
 	for (int i = 0;i < sizeof(kor) / sizeof(int);i++)
-		printf("%d, ", parr[i]);	// kor[i]대신 포인터로 접근할 수 있다.
+		printf("%d, ", pArr[i]);	// kor[i]대신 포인터로 접근할 수 있다.
 	printf("\n");
 
 	/*포인터 변수에 더하는 i는 주소를 i만큼 증가하는 의미가 아니고
 	sizeof(int) * i 만큼 주소를 이동하라는 의미이다.
 	즉 i는 인덱스의 의미이다. */
 	for (int i = 0;i < sizeof(kor) / sizeof(int);i++)
-		printf("%d, ", *(parr + i));
+		printf("%d, ", *(pArr + i));
 	printf("\n");
 }
